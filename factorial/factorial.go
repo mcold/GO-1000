4c739466a5cad225bfab0239b14f65c1c8901a8a
Michael Cold <mholodnuk@gmail.com>
2021-05-25 22:41:55 +0300

    Factorial

---
+++ factorial/factorial.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func factorial(num uint) uint {
+	if num < 1 {
+		panic(fmt.Sprintf("Not correct number!"))
+	}
+	if num == 1 {
+		return 1
+	}
+	return num * factorial(num-1)
+}
+
+func main() {
+	fmt.Println(factorial(5))
+	fmt.Println(factorial(4))
+	fmt.Println(factorial(0))
+}
